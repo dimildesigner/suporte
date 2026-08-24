f198d5dbf1f04887ca535375d501511f700a4830
Edimilson Teixeira <97994730+dimildesigner@users.noreply.github.com>
2026-08-24 15:40:11 -0300

    Upload da versao final dos arquivos

--- src/main/java/com/senai/suporte/SuporteApplication.java
+++ src/main/java/com/senai/suporte/SuporteApplication.java
@@ -1,4 +1,4 @@
-package com.senai.suporte;
+package com.senai.suporte.suporte;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
